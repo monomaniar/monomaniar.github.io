216bd4a1fccfc501067e6250ccb1a1b05439f2e4
monomaniar <wuranbo@126.com>
2013-09-24 00:29:53 +0800

    try not load weibo when mobile

--- js/home.js
+++ js/home.js
@@ -1,6 +1,11 @@
-$(document).load(function(){
+$(".pcload").load(function(){
+        console.log("is mobile;pcload loading.");
+        $(".rightside").remove();
+        $(".section").css("margin-right","5%");
+});
+$(document).ready(function(){
     if (document.WuRanbo.isMobile.any()){
-        console.log("is mobile");
+        console.log("is mobile;ready complete;");
         $(".rightside").remove();
         $(".section").css("margin-right","5%");
     }// if:(document.WuRanbo.isMobile.any())
